f4372f429f64fe02cd9f80dec109f246955f21c7
buildsample <buildsampletest@gmail.com>
2015-07-24 14:17:34 +0530

    Update MemcachedExampleTest.php

--- tests/MemcachedExampleTest.php
+++ tests/MemcachedExampleTest.php
@@ -3,7 +3,7 @@ require_once('MemcachedExample.php');
 class MemcachedExampleTest extends PHPUnit_Framework_TestCase {
   public function testMemcached() {
   	$mem = new MemcachedExample();
-    $this->assertEquals($mem->populate(), 99);
+    $this->assertEquals($mem->populate(), 199);
   }
 }
 ?>
